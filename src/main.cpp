4f264ac6dbfc909bad77128900a5d195620d05dc
Tyler Landtroop <tlandtroop@ufl.edu>
2023-02-15 17:45:07 -0500

    edited readme and worked on main

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,16 @@
 using namespace std;
 
 class BST {
-	
 	public:
 		struct TreeNode {
 			string name;
-			string id;
+			int id;
+			int height;
 			TreeNode* left;
 			TreeNode* right;
-			TreeNode* root;
-			TreeNode() : name(""), id(""), left(nullptr), right(nullptr) {}
+			TreeNode() : name(""), id(0), left(nullptr), right(nullptr) {}
 		};
-
-		TreeNode* root = nullptr;
+		TreeNode* root;
 		
 		// Rotations
 		TreeNode* right(TreeNode* node);
@@ -22,9 +20,9 @@ class BST {
 		TreeNode* leftRight(TreeNode* node);
 
 		// Manipulating functions
-		void insert(string name, string id);
-		void remove(string id);
-		void searchId(string id);
+		TreeNode* insert(TreeNode* root,string name, int id);
+		void remove(int id);
+		void searchId(int id);
 		void searchName(string name);
 
 		// Printing functions
@@ -32,23 +30,19 @@ class BST {
 		void printPreorder();
 		void printPostorder();
 		void printLevelCount(TreeNode* root);
-
 		void removeInorder(int n);
+
 	private:
 		// Helper functions
-		TreeNode* helperInsert(TreeNode* node, string name, string id);
 		void helperInorder(TreeNode* helpRoot);
 		void helperPreorder(TreeNode* helpRoot);
 		void helperPostorder(TreeNode* helpRoot);
 		int height(TreeNode* root);
 		int getBalanceFactor(TreeNode* node);
+		TreeNode* createNode(string name, int id);
 };
 
 // Helpers
-BST::TreeNode* BST::helperInsert(TreeNode* node, string name, string id) { // TODO
-	
-}
-
 void BST::helperInorder(BST::TreeNode* helpRoot) {
 	if (helpRoot == nullptr)
 		cout << "";
@@ -63,7 +57,7 @@ void BST::helperPreorder(BST::TreeNode* helpRoot) {
 	if (helpRoot == nullptr)
 		cout << "";
 	else {
-		cout << helpRoot->name << ", ";
+		cout << helpRoot->id << ", ";
 		helperPreorder(helpRoot->left);
 		helperPreorder(helpRoot->right);
 	}
@@ -92,45 +86,92 @@ int BST::height(TreeNode* root) {
 
 int BST::getBalanceFactor(TreeNode* node) {
 	if (node == nullptr)
-		return -1;
+		return 0;
 	return height(node->left) - height(node->right);
 }
 
+BST::TreeNode* BST::createNode(string name, int id) {
+	TreeNode* node = new TreeNode();
+	node->id = id;
+	node->name = name;
+	node->left = nullptr;
+	node->right = nullptr;
+	node->height = 1;
+
+	return node;
+}
+
 // Rotations
-BST::TreeNode* BST::right(TreeNode* node) {
-	TreeNode* grandchild = node->left->right;
-	TreeNode* newParent =  node->left;
-	node->left = grandchild;
+BST::TreeNode* BST::right(TreeNode* root) {
+	TreeNode* grandchild = root->left->right;
+	TreeNode* newParent =  root->left;
+	newParent->right = root;
+	root->left = grandchild;
 	return newParent;
 }
 
-BST::TreeNode* BST::left(TreeNode* node) {
-	TreeNode* grandchild = node->right->left;
-	TreeNode* newParent =  node->right;
-	node->right = grandchild;
+BST::TreeNode* BST::left(TreeNode* root) {
+	TreeNode* grandchild = root->right->left;
+	TreeNode* newParent =  root->right;
+	root->right = grandchild;
+	root = newParent;
 	return newParent;
 }
 
-BST::TreeNode* BST::rightLeft(TreeNode* node) { // TODO: First rotation on child
-	node = right(node);
-	return left(node);
+BST::TreeNode* BST::rightLeft(TreeNode* root) { // TODO: First rotation on child
+	root = right(root->right);
+	root = left(root);
+	return root;
 }
 
-BST::TreeNode* BST::leftRight(TreeNode* node) { // TODO: First rotation on child
-	node = left(node);
-	return right(node);
+BST::TreeNode* BST::leftRight(TreeNode* root) { // TODO: First rotation on child
+	root = left(root->left);
+	root = right(root);
+	return root;
 }
 
 // Manipulative Functions
-void BST::insert(string name, string id) { // TODO
+BST::TreeNode* BST::insert(TreeNode* root, string name, int id) { // TODO
+	if (root == nullptr) {
+		return createNode(name, id);
+	}
+
+	if (id < root->id) {
+		root->left = insert(root->left, name, id);
+	}
+	else if (id > root->id) {
+		root->right = insert(root->right, name, id);
+	}
+
+	int balanceFactor = getBalanceFactor(root);
+
+	// Left Left
+	if (balanceFactor == 2 && getBalanceFactor(root->left) == 1) {
+		return right(root);
+	}
+	// Right Right
+	if (balanceFactor == -2 && getBalanceFactor(root->right) == -1) {
+		return left(root);
+	}
+
+	// Left Right
+	if (balanceFactor == 2 && getBalanceFactor(root->left) == -1) {
+		return leftRight(root);
+	}
+
+	// Right Left
+	if (balanceFactor == -2 && getBalanceFactor(root->right) == 1) {
+		return rightLeft(root);
+	}
+
+	return root;
+}
+
+void BST::remove(int id) { // TODO
 	
 }
 
-void BST::remove(string id) { // TODO
-	
-}
-
-void BST::searchId(string id) { // TODO
+void BST::searchId(int id) { // TODO
 	
 }
 
@@ -166,8 +207,14 @@ void BST::removeInorder(int n) { // TODO
 int main() {
 	// TODO: Parsing and inputs
 	BST t;
-	t.insert("Andrew", "12345678");
-	t.printInorder();
+	t.root = nullptr;
+	t.root = t.insert(t.root, "A", 10);
+	t.root = t.insert(t.root, "B", 20);
+	t.root = t.insert(t.root, "C", 30);
+	t.root = t.insert(t.root, "D", 40);
+	t.root = t.insert(t.root, "E", 50);
+	t.root = t.insert(t.root, "F", 25);
+	t.printPreorder();
 }
 
 
