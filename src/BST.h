6f43aae4cd7f8cb2749948bfc74bd1054b896839
Tyler Landtroop <tlandtroop@ufl.edu>
2023-02-20 19:53:24 -0500

    completed project and added comments

--- src/BST.h
+++ src/BST.h
@@ -1,47 +1,50 @@
 #include <string>
+#include <vector>
 using namespace std;
 
 class TreeNode {
-	 public:
-		string name;
-		int id;
-		int height;
-		TreeNode* left;
-		TreeNode* right;
-		TreeNode() : name(""), id(0), left(nullptr), right(nullptr) {}
+public:
+  string name;
+  int id;
+  int height;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : name(""), id(0), left(nullptr), right(nullptr) {}
 };
 
 class BST {
-	public:
-		TreeNode* root;
-		
-		// Rotations
-		TreeNode* right(TreeNode* node);
-		TreeNode* left(TreeNode* node);
-		TreeNode* rightLeft(TreeNode* node);
-		TreeNode* leftRight(TreeNode* node);
+public:
+  TreeNode *root;
 
-		// Manipulating functions
-		TreeNode* insert(TreeNode* root,string name, int id);
-		TreeNode* remove(TreeNode* root, int id);
-		void searchId(TreeNode* root, int id);
-		void searchName(TreeNode* root, string name);
+  // Destructor
+  ~BST() { helperDestroy(root); }
 
-		// Printing functions
-		void printInorder(TreeNode* root);
-		void printPreorder(TreeNode* root);
-		void printPostorder(TreeNode* root);
-		void printLevelCount(TreeNode* root);
-		void removeInorder(int n);
+  // Rotations
+  TreeNode *right(TreeNode *node);
+  TreeNode *left(TreeNode *node);
+  TreeNode *rightLeft(TreeNode *node);
+  TreeNode *leftRight(TreeNode *node);
 
-	private:
-		// Helper functions
-		void helperIn(TreeNode* root, vector<string> &vec);
-		void helperPre(TreeNode* root, vector<string> &vec);
-		void helperPost(TreeNode* root, vector<string> &vec);
-		void helperSearchName(TreeNode* root, vector<int> &vec, string name);
-		void helperRemove(TreeNode* root, int &counter, int n);
-		int height(TreeNode* root);
-		int getBalanceFactor(TreeNode* node);
-		TreeNode* createNode(string name, int id);
+  // Manipulating functions
+  TreeNode *insert(TreeNode *root, string name, int id);
+  TreeNode *remove(TreeNode *root, int id);
+  void searchId(TreeNode *root, int id);
+  void searchName(TreeNode *root, string name);
+
+  // Printing functions
+  void printInorder(TreeNode *root);
+  void printPreorder(TreeNode *root);
+  void printPostorder(TreeNode *root);
+  void printLevelCount(TreeNode *root);
+  void removeInorder(TreeNode *root, int n);
+
+private:
+  // Helper functions
+  void helperIn(TreeNode *root, vector<string> &vec);
+  void helperPre(TreeNode *root, vector<string> &vec);
+  void helperPost(TreeNode *root, vector<string> &vec);
+  int height(TreeNode *root);
+  int getBalanceFactor(TreeNode *node);
+  TreeNode *createNode(string name, int id);
+  void helperDestroy(TreeNode *root);
 };
